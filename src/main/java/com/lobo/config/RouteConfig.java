71a8848c8d0939836adba87390fbf14a10c3522e
Melissa Lobo <mellobomel@gmail.com>
2018-02-28 18:47:14 -0300

    first commit

---
+++ src/main/java/com/lobo/config/RouteConfig.java
@@ -0,0 +1,13 @@
+package com.lobo.config;
+
+import org.apache.camel.builder.RouteBuilder;
+
+public class RouteConfig extends RouteBuilder{
+
+	@Override
+	public void configure() throws Exception {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
