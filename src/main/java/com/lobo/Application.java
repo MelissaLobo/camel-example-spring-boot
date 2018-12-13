c802fe907590f755828db8a14fb4fb014a09726e
Melissa Lobo <mellobomel@gmail.com>
2018-12-12 22:24:58 -0200

    add Class with main method, responsible for starting the application

---
+++ src/main/java/com/lobo/Application.java
@@ -0,0 +1,16 @@
+package com.lobo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author Melissa Lobo
+ * Class with main method, responsible for starting the application
+ * 
+ */
+@SpringBootApplication
+public class Application {
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
+}
