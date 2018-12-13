fff01ef5ba88128fe40d6d4361c103c25939b664
Melissa Lobo <mellobomel@gmail.com>
2018-12-12 22:35:55 -0200

    class responsible for application configuration

--- src/main/java/com/lobo/config/RouteConfig.java
+++ src/main/java/com/lobo/config/RouteConfig.java
@@ -1,13 +1,39 @@
 package com.lobo.config;
 
 import org.apache.camel.builder.RouteBuilder;
+import org.apache.camel.component.servlet.CamelHttpTransportServlet;
+import org.apache.camel.model.rest.RestBindingMode;
+import org.springframework.boot.web.servlet.ServletRegistrationBean;
+import org.springframework.context.annotation.Bean;
+import org.springframework.stereotype.Component;
 
-public class RouteConfig extends RouteBuilder{
+/**
+ * @author Melissa Lobo
+ * 
+ * Class responsible for application configuration
+ *
+ */
+@Component
+public class RouteConfig extends RouteBuilder {
+
+	@Bean
+	public ServletRegistrationBean camelServletRegistrationBean() {
+		ServletRegistrationBean registration = new ServletRegistrationBean(new CamelHttpTransportServlet(),
+				"/r/api/v1/*");
+		registration.setName("CamelServlet");
+		return registration;
+	}
 
 	@Override
 	public void configure() throws Exception {
-		// TODO Auto-generated method stub
-		
+		restConfiguration()
+			.dataFormatProperty("prettyPrint", "true")
+			.component("servlet")
+			.bindingMode(RestBindingMode.json)
+			.apiContextPath("/api-doc")
+				.apiProperty("api.title", "API-Main-App")
+				.apiProperty("api.version", "1.0.0")
+				.apiProperty("cors", "true");
 	}
 
 }
